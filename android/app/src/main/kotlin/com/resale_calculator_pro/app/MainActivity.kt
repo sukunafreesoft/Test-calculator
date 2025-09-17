bbfb56f21c0d97d7b8f2b55a2ce0a2ca4637dd7a
sukuna <85hizufu3@gmail.com>
2025-09-18 00:00:40 +0200

    Delete android directory

--- android/app/src/main/kotlin/com/resale_calculator_pro/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.resale_calculator_pro.app
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterFragmentActivity() {
-}
